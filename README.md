0f8cf2e46acc5ef4b1dcfc3339d363178bfabaa3
Sergey Zhumatiy <serg@parallel.ru>
2016-04-27 18:39:12 +0300

    - version 1.0

---
+++ README.md
@@ -0,0 +1,102 @@
+The Rubysaver
+=============
+
+Features
+--------
+
+Works as Xscreensaver module. Shows current weather, forecast and tomorrow forecast. Shows clock. If your mpris.MediaPlayer2 compatible player (most of popular players) is playing, shows current track title and performer.
+
+After sunset can display all darker (default) to less disturb you. If you don't want it, set weather_image_alpha and weather_image_noalpha to same value (e.g. 255.0).
+
+Pre-requisites
+--------------
+
+- Ruby 1.9.1+
+- ruby gems: gtk2, dbus
+- xscreensaver or compatible
+
+You can install them via packages. E.g. for debian/ubuntu: `apt-get install ruby ruby-gtk2 ruby-cairo ruby-dbus ruby-glib2 ruby-pango`
+
+Or you can install rbenv (`curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash`), then
+install ruby (e.g. 2.2.0: `rbenv install 2.2.0; `rbenv global 2.2.0`), and gems: `gem install gtk2; gem install dbus; gem install rcairo`.
+
+Installing
+----------
+
+Just `mkdir /opt/rubysaver` (you can choose another directory), then copy rs.rb and iconsbest.com-icons there:
+`cp -r rs.rb iconsbest.com-icons/ /opt/rubysaver`. Make rs.rb executable: `chmod a+x /opt/rubysaver/rs.rb`.
+Create ~/.config/rubysaver.conf, and fil your localtion, e.g.:
+
+```
+place: Tokyo,JP
+```
+
+Edit ~/.xscreensaver, find line 'programs:' and put just after it: `/opt/rubysaver/rs.rb \n\`. Modify lines:
+
+```
+mode:           one
+selected:       0
+```
+
+That's all! Just lock your screen or start xscreensaver. If you're using xscreensaver compatible saver,
+specify /opt/rubysaver/rs.rb as active module.
+
+
+Customizing
+-----------
+
+You can change many options in ~/.config/rubysaver.conf: (yaml format)
+
+```
+# path to icons
+icon_path: /opt/rubysaver/iconsbest.com-icons
+# name for weather font
+font_face: Sans Serif
+# weight for weather font
+font_weight: bold
+# font size for forecasts
+weather_font_size: 20
+# font size for current weather
+weather_big_font_size: 38
+# font name for clock
+font_face_clock: Sans Serif
+# font weight for clock
+font_weight_clock: ultrabold
+# font size for clock
+weather_clock_font_size: 42
+# font name for 'now playing' string. Use ONLY monosized fonts!
+font_face_play: Mono
+# font weight for 'now playing' string
+font_weight_play: normal
+# font size for 'now playing' string
+play_font_size: 40
+# font size for REQUIRED reference to Yahoo.
+yahoo_font_size: 8
+
+# alpha level for tinted image (after sunset)
+weather_image_alpha: 100.0
+# alpha level for normal image (after sunrise)
+weather_image_noalpha: 255.0
+# speed of sliding in x and y direction
+xspeed: 2
+yspeed: 2
+
+# your place (see Yahoo!Weather)
+place: Tokyo,JP
+# if Yahoo finds several cities, select yours by right index
+place_index:1
+# currently only english and russian are supported. Send my pull requests for more!
+lang: english
+
+# speed of 'now playing' scrolling
+np_speed: 5,
+# maximum 'now playing' string length
+max_play_len: 48,
+# weather update interval in seconds. Don't ask Yahoo often!
+update_interval: 1200,
+# sunrise time, if yahoo didn't say it
+min_tint_hour: 6,
+# sunset time, if yahoo didn't say it
+max_tint_hour: 22,
+```
+
---
+++ rs.rb
@@ -0,0 +1,667 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+
+require 'gtk2'
+require 'dbus'
+require 'json'
+require 'net/https'
+require 'yaml'
+
+SIGNAL_DRAW="expose_event"
+
+# for gtk3
+#SIGNAL_DRAW="draw"
+
+DEF_IMG_PATH="/opt/rubysaver/iconsbest.com-icons/"
+NA_IMAGE=50
+TMOUT=100
+X_SPEED=2
+Y_SPEED=2
+NP_SPEED=5
+MAX_PLAY_LENGTH=48
+
+$weather=nil
+$drawing_area=nil
+
+class RubyApp < Gtk::Window
+
+  def initialize
+    super
+    
+    set_title "Xscreensaver module"
+    signal_connect "destroy" do 
+      Gtk.main_quit 
+    end
+
+
+    init_ui
+    realize
+
+    ident = ENV['XSCREENSAVER_WINDOW']
+    #warn "ident=#{ident}"
+    if not ident.nil?
+      self.window=Gdk::Window.foreign_new(ident.to_i(16))
+      self.window.set_events (Gdk::Event::EXPOSURE_MASK | Gdk::Event::STRUCTURE_MASK)
+      #warn "win=#{self.window}"
+    end
+    x, y, width, height, depth = window.geometry
+
+    set_default_size width, height
+    set_window_position :center
+
+    show_all
+  end
+
+  def init_ui
+
+    $drawing_area = @darea = Gtk::DrawingArea.new  
+    @darea.signal_connect SIGNAL_DRAW do |widget,event|
+      on_draw widget
+    end
+
+    add @darea 
+  end
+
+  def on_draw widget
+    cr = widget.window.create_cairo_context
+
+    do_draw cr, widget
+  end
+
+  def do_draw cr, widget
+    x, y, width, height, depth = window.geometry
+    cr.save do
+      cr.set_source_color(Gdk::Color.new(0,0,0))
+      cr.gdk_rectangle(Gdk::Rectangle.new(0, 0, width, height))
+      cr.fill
+    end
+
+    unless $weather.nil?
+      $weather.draw_weather cr, width, height
+    end
+
+  end
+end
+
+
+
+class Weather
+
+  DEF_CONF={
+    'icon_path'=>DEF_IMG_PATH,
+    'weather_font_size'=>20,
+    'weather_big_font_size'=>38,
+    'weather_clock_font_size'=>50,
+    'yahoo_font_size'=>8,
+    'weather_image_alpha'=>100.0,
+    'weather_image_noalpha'=>255.0,
+    'font_face'=>"Sans Serif",
+    'font_weight'=>'bold',
+    'font_face_clock'=>"Sans Serif",
+    'font_weight_clock'=>'ultrabold',
+    'font_face_play'=>"Mono",
+    'font_weight_play'=>'normal',
+    'play_font_size'=>40,
+    'np_speed'=>NP_SPEED,
+    'max_play_len'=>MAX_PLAY_LENGTH,
+    'update_interval'=>1200,
+    'min_tint_hour'=>6,
+    'max_tint_hour'=>22,
+    'xspeed'=>X_SPEED,
+    'yspeed'=>Y_SPEED,
+    'lang'=>'russian',
+    'place' => "Moscow,RU",
+    'place_index'=>1,
+  }
+
+  def initialize(conf=nil)
+    @bg_colour=Gdk::Color.new(0,0,0)
+    @face_colour=Gdk::Color.new 250,250,250
+    @marks_colour=Gdk::Color.new 30,30,30
+    @fill_colour=Gdk::Color.new 255,0,0
+    @line_colour=Gdk::Color.new 0,0,255
+
+    cnf=DEF_CONF
+    begin
+      cnf2=YAML.load(File.read(conf))
+      cnf.merge! cnf2
+    rescue Exception => e
+      warn "Cannot load config! #{e}"
+    end                
+    cnf.each do |k,v|
+      instance_variable_set "@#{k}", v
+      #warn "#{k} -> #{v}"
+    end
+
+    @weather_image_h=120
+    @weather_image_w=120
+
+    @now_playing_height=0
+    @time_now=Time.now
+    @WEATHER_IMAGE_CHECKERS_COLOR=0
+    @new_update=Time.new(0)
+    @x=@y=0
+    @weather_width=0
+    @colon_color=[0.1, 0.1, 0.0, 0.0, 1.0,
+                  0.5, 0.8, 0.8, 0.8, 0.8]
+
+    @weather_images_cache=[]
+    0.upto(47) do |i|
+      loader = Gdk::PixbufLoader.new
+      File.open("#{@icon_path}/%02i.gif" % i, "rb") do |f|
+        loader.last_write(f.read)
+      end
+      @weather_images_cache[i] = loader.pixbuf
+    end
+    loader = Gdk::PixbufLoader.new
+    File.open("#{@icon_path}/na.gif", "rb") do |f|
+      loader.last_write(f.read)
+    end
+    @weather_images_cache[NA_IMAGE] = loader.pixbuf
+
+    @bus = DBus::SessionBus.instance
+
+    @bus.proxy.ListNames[0].select do |service_name|
+      service_name =~ /^org.mpris.MediaPlayer2/
+    end.collect do |service_name|
+      obj=@bus.service(service_name).object("/org/mpris/MediaPlayer2")
+      obj.introspect
+      #warn "Set on #{obj}"
+      obj['org.freedesktop.DBus.Properties'].on_signal(@bus, "PropertiesChanged") do |player,meta,b|
+        on_track_change player,meta,b
+      end
+    end
+
+    get_track_name
+  end
+
+  def get_track_name
+    meta=nil
+    @bus.proxy.ListNames[0].select do |service_name|
+      service_name =~ /^org.mpris.MediaPlayer2/
+    end.collect do |service_name|
+      obj=@bus.service(service_name).object("/org/mpris/MediaPlayer2")
+      obj.introspect
+      player=obj['org.mpris.MediaPlayer2.Player']
+      return if player.nil?
+      meta=player['Metadata']
+      if meta.nil?
+        @playing=false
+      else
+        parse_meta meta
+        @playing=true
+      end
+    end
+  end
+
+  def parse_meta m
+    @title=m["xesam:title"].to_s
+    @artist=m["xesam:artist"].to_a.join(';').to_s
+    @url=m["xesam:url"].to_s.gsub /.*\//, ''
+    @url.gsub! '.mp3', ''        
+  end
+
+  def on_track_change player,meta,b
+    if meta["PlaybackStatus"].to_s=="Paused"
+      @playing=false
+      return
+    end
+    m=meta["Metadata"]
+    if m.nil?
+      get_track_name
+      return
+    end
+    @playing=true
+
+    parse_meta m
+  end
+
+  def read_now_playing
+    return nil unless @playing
+    return nil if "#{@title}#{@artist}" == ''
+    "#{@title} // #{@artist}"
+  end
+
+  def show_play_line cr, x,y, text, sh=1.0
+    a=get_image_alpha
+    r=(@face_colour.red*a/255).to_i
+    g=(@face_colour.green*a/255).to_i
+    b=(@face_colour.blue*a/255).to_i
+    cut=4
+    max_len=0
+    if text.length < 8
+      cut=(text.length / 2).to_i
+    elsif text.length > @max_play_len
+      max_len=@max_play_len
+    end
+    txt="<span face=\"#{@font_face_play}\" size=\"#{@play_font_size*1000}\" weight=\"bold\">w</span>"
+    l=cr.create_pango_layout
+    l.markup=txt
+    e,ext=l.pixel_extents
+    width_one=ext.width
+    txt="<span face=\"#{@font_face_play}\" size=\"#{@play_font_size*1000}\" weight=\"bold\">"##{@font_weight_play}\">"
+    1.upto(cut) do |i|
+      color=(r/cut*(i-sh)).to_i*65536+(g/cut*(i-sh)).to_i*256+(b/cut*(i-sh)).to_i
+      txt="#{txt}<span foreground=\"##{'%06X' % color}\">#{text[i-1]}</span>"
+    end
+    color=r*65536+g*256+b
+    txt="#{txt}<span foreground=\"##{'%06X' % color}\">#{text[cut .. max_len-cut-1]}</span>"
+    cut.downto(1) do |i|
+      color=(r/cut*(i+sh-1)).to_i*65536+(g/cut*(i+sh-1)).to_i*256+(b/cut*(i+sh-1)).to_i
+      txt="#{txt}<span foreground=\"##{'%06X' % color}\">#{text[max_len-i]}</span>"
+    end
+    txt="#{txt}</span>"
+    l=cr.create_pango_layout
+    l.markup=txt
+    cr.move_to x-width_one*sh,y
+    cr.show_pango_layout l
+  end
+
+  def draw_now_playing cr, text_x,text_y
+
+    text=read_now_playing
+
+    return if text.nil?
+
+    np_read,sh=make_cycled text
+
+    first_char=np_read[0]
+
+    show_play_line cr, text_x, text_y, np_read[0 .. -1], sh
+  end
+
+  def draw_clock cr, x, y
+    txt="<span face=\"#{@font_face_clock}\" size=\"#{@weather_clock_font_size*1000}\" weight=\"#{@font_weight_clock}\">"
+
+    time_str1=get_time_str1
+    time_str2=get_time_str2
+
+    col=@colon_color[@time_now.usec/100000]*get_image_alpha/256
+    a=get_image_alpha/256
+    color="#{'%02X' % (@face_colour.red*a).to_i}#{'%02X' % (@face_colour.green*a).to_i}#{'%02X' % (@face_colour.blue*a).to_i}"
+    color2="#{'%02X' % (@face_colour.red*col).to_i}#{'%02X' % (@face_colour.green*col).to_i}#{'%02X' % (@face_colour.blue*col).to_i}"
+
+    txt="#{txt}<span foreground=\"##{color}\">#{time_str1}</span><span foreground=\"##{color2}\">:</span><span foreground=\"##{color}\">#{time_str2}</span></span>"
+    l=cr.create_pango_layout
+    l.markup=txt
+
+    e,extents=l.pixel_extents
+    @clock_width=extents.width
+    @clock_height=extents.height
+
+    cr.move_to x, y
+    cr.show_pango_layout l
+
+  end
+
+  def draw_picture cr
+    w_now=w_today=w_tomorrow="Unknown..."
+    if @now_celsium
+      w_now="%+3d %s" % [@now_celsium, @now_weather_text1]
+      w_today="%+3d..%+3d %s" % [@today_celsium_low, @today_celsium_high, @today_forecast_text1]
+      w_tomorrow="%+3d..%+3d %s" % [@tomorrow_celsium_low, @tomorrow_celsium_high, @tomorrow_forecast_text1]        
+    end
+
+    # @now_image_index ||= 1
+    # @today_image_index ||= 1
+    # @tomorrow_image_index ||= 1
+
+    image = @weather_images_cache[@now_image_index]
+    image1 = @weather_images_cache[@today_image_index]
+    image2 = @weather_images_cache[@tomorrow_image_index]
+
+    img=image.composite @weather_image_w, @weather_image_h,
+    Gdk::Pixbuf::InterpType::BILINEAR, get_image_alpha,
+    @weather_image_w, 
+    @WEATHER_IMAGE_CHECKERS_COLOR, @WEATHER_IMAGE_CHECKERS_COLOR
+
+    cr.set_source_pixbuf img, @x, @y
+
+    cr.paint
+
+    x=@x
+    y=@y+@weather_image_h
+
+    img=image1.composite @weather_image_w/2, @weather_image_h/2,
+    Gdk::Pixbuf::InterpType::BILINEAR,
+    get_image_alpha,
+    @weather_image_w,
+    @WEATHER_IMAGE_CHECKERS_COLOR, @WEATHER_IMAGE_CHECKERS_COLOR
+
+    cr.set_source_pixbuf img, x, y
+
+    cr.paint
+
+    y=@y+@weather_image_h*3/2;
+    img=image2.composite @weather_image_w/2, @weather_image_h/2,
+    Gdk::Pixbuf::InterpType::BILINEAR,
+    get_image_alpha,
+    @weather_image_w,
+    @WEATHER_IMAGE_CHECKERS_COLOR, @WEATHER_IMAGE_CHECKERS_COLOR
+
+    cr.set_source_pixbuf img, x, y
+
+    cr.paint
+
+    a=get_image_alpha/256
+    color="##{'%02X' % (@face_colour.red*a).to_i}#{'%02X' % (@face_colour.green*a).to_i}#{'%02X' % (@face_colour.blue*a).to_i}"
+
+    yahoo="<span size=\"#{@yahoo_font_size*1000}\">\nYahoo!Weather</span>"
+    txt="<span face=\"#{@font_face}\" size=\"#{@weather_big_font_size*1000}\" weight=\"#{@font_weight}\" foreground=\"#{color}\">#{w_now}\n#{@now_weather_text2}#{yahoo}</span>"
+    l=cr.create_pango_layout
+    l.markup=txt
+
+    e,extents=l.pixel_extents
+    now_t_w=extents.width
+    cr.move_to @x+@weather_image_w, @y
+    cr.show_pango_layout l
+
+    txt="<span face=\"#{@font_face}\" size=\"#{@weather_font_size*1000}\" weight=\"#{@font_weight}\" foreground=\"#{color}\">#{w_today}\n#{@today_forecast_text2}</span>"
+    l=cr.create_pango_layout
+    l.markup=txt
+
+    e,extents=l.pixel_extents
+    today_f_w=extents.width
+    cr.move_to @x+@weather_image_w, @y+@weather_image_h
+    cr.show_pango_layout l
+
+    txt="<span face=\"#{@font_face}\" size=\"#{@weather_font_size*1000}\" weight=\"#{@font_weight}\" foreground=\"#{color}\">#{w_tomorrow}\n#{@tomorrow_forecast_text2}</span>"
+    l=cr.create_pango_layout
+    l.markup=txt
+
+    e,extents=l.pixel_extents
+    tomorrow_f_w=extents.width
+    cr.move_to @x+@weather_image_w, @y+@weather_image_h*3/2
+    cr.show_pango_layout l
+
+    @weather_width=[now_t_w,today_f_w,tomorrow_f_w].max+@weather_image_w
+
+  end
+
+  def draw_weather cr, width, height
+
+    @time_now=Time.now
+
+    count_clock_height cr if @weather_height.nil?
+    try_update
+
+    draw_picture cr
+    draw_clock cr, @x, @y+@weather_image_h*2
+    draw_now_playing cr, @x, @y+@weather_image_h*2.5
+
+
+
+    @x+=@xspeed
+    @y+=@yspeed
+    max_width=[@clock_width,@weather_width].max
+    if @x+max_width>width || @x<0
+      @xspeed=-@xspeed
+    end
+    if @y+@weather_image_w*2.5>height || @y<0
+      @yspeed=-@yspeed
+    end
+
+  end
+
+
+  def get_image_alpha
+    ((@time_now.hour*60+@time_now.min<=@sunrise_h*60+@sunrise_m)||
+      (@time_now.hour*60+@time_now.min>=@sunset_h*60+@sunset_m)) ?
+    @weather_image_alpha : @weather_image_noalpha
+  end
+
+  def get_time_str
+    set_locale
+    @time_now.strftime "%a %d %b %H %M"
+  end
+
+  def get_time_str1
+    #set_locale
+    @time_now.strftime "%a %d %b %H"
+  end
+
+  def get_time_str2
+    #set_locale
+    @time_now.strftime "%M"
+  end
+
+  def make_cycled text
+    text2="#{text} | #{text} | "
+    len=text.size+3
+
+    shift=10*(@time_now.sec+(@time_now.usec/1000000.0))/@np_speed
+    pos=shift % (len>3 ? len : 1)
+    shift-=shift.to_i.to_f
+    return text2[pos .. pos+len],shift
+  end
+
+
+  def count_clock_height cr
+
+    cr.select_font_face @font_face_CLOCK, @CAIRO_FONT_SLANT_NORMAL,
+    @font_weight
+    cr.set_font_size @weather_clock_font_size
+    cr.set_font_options(Cairo::FontOptions.new.set_antialias(Cairo::ANTIALIAS_SUBPIXEL))        
+
+    extents=cr.text_extents get_time_str
+    @weather_height=@weather_image_h*2+extents.height
+  end
+
+  def get_time_str
+    Time.now.strftime "%a %d %b %H %M"
+  end
+
+  def try_update
+    if @new_update < @time_now
+      update_weather
+      @new_update=@time_now+@update_interval
+    end
+  end
+
+  def update_weather
+    uri=URI.parse("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(#{@place_index})%20where%20text%3D%22#{@place}%22)%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    answer=nil
+    begin
+      a = http.request(request)
+      if a.is_a? Net::HTTPSuccess
+        answer=JSON.load(a.body)
+      else
+        warn "Bad answer: #{a.code}/#{a.message}"
+        return
+      end
+      @sunrise=answer["query"]["results"]["channel"]["astronomy"]["sunrise"]
+      @sunset=answer["query"]["results"]["channel"]["astronomy"]["sunset"]
+
+      if /(\d+):(\d+)/ =~ @sunrise
+        @sunrise_h=$1.to_i
+        @sunrise_m=$2.to_i
+      else
+        @sunrise_h=@min_tint_hour
+        @sunrise_m=0
+      end
+      if /(\d+):(\d+)/ =~ @sunset
+        @sunset_h=$1.to_i+12
+        @sunset_m=$2.to_i
+      else
+        @sunset_h=@min_tint_hour
+        @sunset_m=0
+      end
+
+      @now_celsium=answer["query"]["results"]["channel"]["item"]["condition"]["temp"]
+      @now_image_index=answer["query"]["results"]["channel"]["item"]["condition"]["code"].to_i
+      @now_weather_text1=COND[@lang][@now_image_index][0]
+      @now_weather_text2=COND[@lang][@now_image_index][1]
+
+      @today_celsium_low=answer["query"]["results"]["channel"]["item"]["forecast"][0]["low"]
+      @today_celsium_high=answer["query"]["results"]["channel"]["item"]["forecast"][0]["high"]
+      @today_image_index=answer["query"]["results"]["channel"]["item"]["forecast"][0]["code"].to_i
+      @today_forecast_text1=COND[@lang][@today_image_index][0]
+      @today_forecast_text2=COND[@lang][@today_image_index][1]
+
+      @tomorrow_celsium_low=answer["query"]["results"]["channel"]["item"]["forecast"][1]["low"]
+      @tomorrow_celsium_high=answer["query"]["results"]["channel"]["item"]["forecast"][1]["high"]
+      @tomorrow_image_index=answer["query"]["results"]["channel"]["item"]["forecast"][1]["code"].to_i
+      @tomorrow_forecast_text1=COND[@lang][@tomorrow_image_index][0]
+      @tomorrow_forecast_text2=COND[@lang][@tomorrow_image_index][1]
+
+    rescue
+      @sunrise_h=@min_tint_hour
+      @sunrise_m=0
+      @sunset_h=@min_tint_hour
+      @sunset_m=0
+      @now_celsium=-100
+      @now_image_index=NA_IMAGE
+      @now_weather_text1='???'
+      @now_weather_text2='???'
+
+      @today_celsium_low=-100
+      @today_celsium_high=-100
+      @today_image_index=NA_IMAGE
+      @today_forecast_text1='???'
+      @today_forecast_text2='???'
+
+      @tomorrow_celsium_low=-100
+      @tomorrow_celsium_high=-100
+      @tomorrow_image_index=NA_IMAGE
+      @tomorrow_forecast_text1='???'
+      @tomorrow_forecast_text2='???'
+    end
+  end
+
+  private
+  COND={
+    'english' =>
+    [
+      ["tornado",""],
+      ["tropical","storm"],
+      ["hurricane",""],
+      ["severe","thunderstorms"],
+      ["thunderstorms",""],
+      ["mixed rain","and snow"],
+      ["mixed rain","and sleet"],
+      ["mixed snow","and sleet"],
+      ["freezing","drizzle"],
+      ["drizzle",""],
+
+      ["freezing","rain"],
+      ["showers",""],
+      ["showers",""],
+      ["snow","flurries"],
+      ["light snow showers",""],
+      ["blowing snow",""],
+      ["snow",""],
+      ["hail",""],
+      ["sleet",""],
+      ["dust",""],
+
+      ["foggy",""],
+      ["haze",""],
+      ["smoky",""],
+      ["blustery",""],
+      ["windy",""],
+      ["cold",""],
+      ["cloudy",""],
+      ["mostly cloudy",""],
+      ["mostly cloudy",""],
+      ["partly cloudy",""],
+
+      ["partly cloudy",""],
+      ["clear",""],
+      ["sunny",""],
+      ["fair",""],
+      ["fair",""],
+      ["mixed rain and hail",""],
+      ["hot",""],
+      ["isolated thunderstorms",""],
+      ["scattered thunderstorms",""],
+      ["scattered thunderstorms",""],
+
+      ["scattered showers",""],
+      ["heavy snow",""],
+      ["scattered snow showers",""],
+      ["heavy snow",""],
+      ["partly cloudy",""],
+      ["thundershowers",""],
+      ["snow showers",""],
+      ["isolated thundershowers",""]
+    ],
+    'russian' =>
+    [
+
+      ["Торнадо",""],
+      ["Шторм",""],
+      ["Ураган",""],
+      ["Временами","грозы"],
+      ["Грозы",""],
+      ["Снег","с дождём"],
+      ["Дождь,","слякоть"],
+      ["Снег,","слякоть"],
+      ["Морось","с гололедицей"],
+      ["Моросящий","дождь"],
+
+      ["Дождь","с гололедицей"],
+      ["Дождь",""],
+      ["Дождь",""],
+      ["Снегопад",""],
+      ["Лёгкий","снег"],
+      ["Ветер","со снегом"],
+      ["Снег",""],
+      ["Град",""],
+      ["Слякоть",""],
+      ["Пыль",""],
+
+      ["Туман",""],
+      ["Лёгкий","туман"],
+      ["Дым",""],
+      ["Ветренно",""],
+      ["Ветер",""],
+      ["Холод",""],
+      ["Облачно",""],
+      ["Преимущественно","облачно"],
+      ["Преимущественно","облачно"],
+      ["Временами","облачно"],
+
+      ["Временами","облачно"],
+      ["Ясно",""],
+      ["Солнечно",""],
+      ["Преимущественно","ясно"],
+      ["Преимущественно","ясно"],
+      ["Дождь","с градом"],
+      ["Жара",""],
+      ["Местами","грозы"],
+      ["Местами","грозы"],
+      ["Местами","грозы"],
+
+      ["Местами","дожди"],
+      ["Снегопад",""],
+      ["Местами","снег с дождём"],
+      ["Снегопад",""],
+      ["Местами","облачно"],
+      ["Гроза",""],
+      ["Снег","с дождём"],
+      ["Местами ","грозы"]
+    ]
+  }
+end
+
+
+Gtk.init
+DBus::SessionBus.instance.glibize
+
+conf_file="#{ENV['HOME']}/.config/rubysaver.conf"
+$weather=Weather.new(conf_file)
+window = RubyApp.new
+
+GLib::Timeout.add(TMOUT){
+    $drawing_area.queue_draw
+    true
+}
+
+Gtk.main
+
